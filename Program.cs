529f8d58d393f95a040a8d5cb81bf1e9e050c6e6
Kevin Bost <kitokeboo@gmail.com>
2020-08-24 08:51:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Snoop.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = Process.GetProcessesByName("MaterialDesignDemo").Single();
+
+            Interactive.Interactive.Start(p.Id);
+        }
+    }
+}
